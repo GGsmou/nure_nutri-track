019eba9387decd33885cecefffa63ceae3fe3270
Nikita Dzhezhela <dzhezhela.nikita@gmail.com>
2024-05-28 17:42:38 +0300

    feat: added first landing section.

--- fe/src/routes/Landing.tsx
+++ fe/src/routes/Landing.tsx
@@ -1,3 +1,4 @@
+import { PlayCircleOutline } from "@mui/icons-material";
 import {
   Box,
   Button,
@@ -7,7 +8,6 @@ import {
   createTheme,
 } from "@mui/material";
 import { Link } from "react-router-dom";
-
 const theme = createTheme({
   palette: {
     primary: { main: "#54BD95" },
@@ -22,67 +22,128 @@ export const Landing = () => {
           minHeight="100vh"
           sx={{
             background:
-              "radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(82,189,170,0.1) 100%)",
+              "radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(82,189,170,0.3) 100%)",
           }}
         >
-          <header>
-            <Container
-              sx={{
-                paddingTop: "20px",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "space-between",
-                width: "100%",
-              }}
-            >
-              <Typography
-                variant="h1"
-                fontSize={50}
-                fontWeight="bold"
-                color="#54BD95"
-              >
-                NutriTrack
-              </Typography>
+          <Container>
+            <header>
               <Box
                 sx={{
-                  gap: 4,
+                  paddingTop: "20px",
                   display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  width: "100%",
                 }}
               >
-                <Button
-                  sx={{
-                    color: "black",
-                    textTransform: "none",
-                  }}
-                  size="large"
+                <Typography
+                  variant="h1"
+                  fontSize={50}
+                  fontWeight="bold"
+                  color="#54BD95"
                 >
-                  Home
-                </Button>
-                <Button
+                  NutriTrack
+                </Typography>
+                <Box
                   sx={{
-                    color: "black",
-                    textTransform: "none",
+                    gap: 4,
+                    display: "flex",
                   }}
-                  size="large"
                 >
-                  Download
-                </Button>
-                <Link to="/auth">
                   <Button
-                    variant="contained"
-                    color="primary"
                     sx={{
-                      color: "white",
+                      color: "black",
                       textTransform: "none",
                     }}
                     size="large"
                   >
-                    Sign Up
+                    Home
                   </Button>
-                </Link>
+                  <Button
+                    sx={{
+                      color: "black",
+                      textTransform: "none",
+                    }}
+                    size="large"
+                  >
+                    Download
+                  </Button>
+                  <Link to="/auth">
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      sx={{
+                        color: "white",
+                        textTransform: "none",
+                      }}
+                      size="large"
+                    >
+                      Sign Up
+                    </Button>
+                  </Link>
+                </Box>
               </Box>
-            </Container>
-          </header>
+            </header>
+            <Box
+              sx={{
+                marginTop: "130px",
+                display: "flex",
+                justifyContent: "space-between",
+                gap: "35px",
+              }}
+            >
+              <Box
+                maxWidth="555px"
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Typography
+                  variant="h1"
+                  fontWeight="bold"
+                  fontSize="80px"
+                  color="black"
+                >
+                  Empowering your health, one calorie at a time
+                </Typography>
+                <Box sx={{ display: "flex", gap: "70px" }}>
+                  <Button
+                    size="large"
+                    variant="contained"
+                    sx={{
+                      color: "white",
+                      textTransform: "none",
+                    }}
+                  >
+                    Try now
+                  </Button>
+                  <Button
+                    startIcon={<PlayCircleOutline />}
+                    size="large"
+                    sx={{
+                      color: "black",
+                      textTransform: "none",
+                    }}
+                  >
+                    About us
+                  </Button>
+                </Box>
+              </Box>
+              <img src="/landing_1.png" alt="calories" />
+            </Box>
+            <Box mt="90px">
+              <Typography
+                variant="h2"
+                textAlign="center"
+                color="#191A15"
+                fontWeight="semi-bold"
+              >
+                More than 10,000 happy users
+              </Typography>
+            </Box>
+          </Container>
         </Box>
       </div>
     </ThemeProvider>
