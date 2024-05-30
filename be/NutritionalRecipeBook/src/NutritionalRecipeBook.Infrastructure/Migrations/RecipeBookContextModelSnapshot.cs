cabc5a62da028121581a0b01ddca600c422d857f
Daryna <darinahanina04@gmail.com>
2024-05-30 15:02:03 +0200

    Added list of ids for CreateRecipeRequest for NewIngredientIds

--- be/NutritionalRecipeBook/src/NutritionalRecipeBook.Infrastructure/Migrations/RecipeBookContextModelSnapshot.cs
+++ be/NutritionalRecipeBook/src/NutritionalRecipeBook.Infrastructure/Migrations/RecipeBookContextModelSnapshot.cs
@@ -318,6 +318,43 @@ namespace NutritionalRecipeBook.Infrastructure.Migrations
                     b.HasKey("Id");
 
                     b.ToTable("Ingredients");
+
+                    b.HasData(
+                        new
+                        {
+                            Id = new Guid("07635319-6b55-4cb2-ba37-611f771aeb57"),
+                            Name = "Tomato"
+                        },
+                        new
+                        {
+                            Id = new Guid("b3dc04e0-a4c9-4929-957d-c509e9a1b4c5"),
+                            Name = "Lettuce"
+                        },
+                        new
+                        {
+                            Id = new Guid("28dee073-6556-46bb-9055-71f9d536e947"),
+                            Name = "Cucumber"
+                        },
+                        new
+                        {
+                            Id = new Guid("bfa0ca70-344c-4641-8685-dc834e5a5f8b"),
+                            Name = "Carrot"
+                        },
+                        new
+                        {
+                            Id = new Guid("7c753fe6-5409-444c-95c6-e5adaf71ebc6"),
+                            Name = "Onion"
+                        },
+                        new
+                        {
+                            Id = new Guid("a515d859-e2fa-4bd7-803c-f169281b50b4"),
+                            Name = "Garlic"
+                        },
+                        new
+                        {
+                            Id = new Guid("e018ddca-c0dc-4a6c-99aa-ee54714f0560"),
+                            Name = "Bell Pepper"
+                        });
                 });
 
             modelBuilder.Entity("NutritionalRecipeBook.Domain.Entities.Recipe", b =>
