ed860acc4e8888b15e4b4806db55174acc6872ad
Daryna <darinahanina04@gmail.com>
2024-05-30 15:01:31 +0200

    Added seed data for Ingredients

---
+++ be/NutritionalRecipeBook/src/NutritionalRecipeBook.Infrastructure/Helpers/ModelBuilderExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NutritionalRecipeBook.Domain.Entities;
+
+namespace NutritionalRecipeBook.Infrastructure.Helpers;
+
+public static class ModelBuilderExtensions
+{
+    public static void Seed(this ModelBuilder modelBuilder)
+    {
+        var deliveryInfos = new List<Ingredient>()
+        {
+            new Ingredient("Tomato"),
+            new Ingredient("Lettuce"),
+            new Ingredient("Cucumber"),
+            new Ingredient("Carrot"),
+            new Ingredient("Onion"),
+            new Ingredient("Garlic"),
+            new Ingredient("Bell Pepper")
+        };
+        
+        modelBuilder.Entity<Ingredient>().HasData(deliveryInfos);
+    }
+}
