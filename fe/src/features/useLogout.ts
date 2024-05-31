2606722f000da187532e949df554ba8b387eea56
Nikita Dzhezhela <dzhezhela.nikita@gmail.com>
2024-05-31 16:34:37 +0300

    feat: added logout logic.

---
+++ fe/src/features/useLogout.ts
@@ -0,0 +1,16 @@
+import { useMutation } from "react-query";
+import { useNavigate } from "react-router-dom";
+import { authService } from "../utils/authService";
+
+export const useLogout = () => {
+  const navigate = useNavigate();
+  return useMutation({
+    mutationKey: "user",
+    mutationFn: async () => {
+      //await fetchAbstract("identity/logout", "post");
+      authService.removeAuthInfo();
+      navigate("/auth");
+      return;
+    },
+  });
+};
