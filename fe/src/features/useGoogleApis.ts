e81ab51e9841976684cea57227978849eefa935f
Nikita Dzhezhela <dzhezhela.nikita@gmail.com>
2024-06-05 20:30:32 +0300

    fix: added script tag to the index.html file.

--- fe/src/features/useGoogleApis.ts
+++ fe/src/features/useGoogleApis.ts
@@ -1,5 +1,4 @@
 import { calendar_v3 } from "googleapis";
-import load from "load-script";
 import { useEffect, useState } from "react";
 import { clientId } from "../utils/consts";
 
@@ -8,24 +7,23 @@ const DISCOVERY_DOC =
 
 export const useGoogleApis = () => {
   const [calendar, setCalendar] = useState<calendar_v3.Calendar | null>(null);
+
   useEffect(() => {
-    load("https://apis.google.com/js/api.js", async () => {
-      gapi.load("client", async () => {
-        await gapi.client.init({
-          clientId: clientId,
-          discoveryDocs: [DISCOVERY_DOC],
-        });
-
-        gapi.client.setToken({
-          access_token: "<ACCESS_TOKEN>",
-        });
-
-        setCalendar(
-          gapi.client[
-            "calendar" as unknown as keyof typeof gapi.client
-          ] as unknown as calendar_v3.Calendar,
-        );
+    gapi.load("client", async () => {
+      await gapi.client.init({
+        clientId: clientId,
+        discoveryDocs: [DISCOVERY_DOC],
       });
+
+      gapi.client.setToken({
+        access_token: "<ACCESS_TOKEN>",
+      });
+
+      setCalendar(
+        gapi.client[
+          "calendar" as unknown as keyof typeof gapi.client
+        ] as unknown as calendar_v3.Calendar,
+      );
     });
   });
 
